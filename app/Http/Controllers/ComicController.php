974b7654371fe0965ee7ed704f08e99cf2d1521c
anto24s96 <anto24s96@gmail.com>
2024-02-20 18:03:37 +0100

    [BONUS 2] exercise completed

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
+use App\Http\Requests\UpgradeComicRequest;
 use App\Models\Comic;
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Validator;
+/* use Illuminate\Support\Facades\Validator; */
 
 class ComicController extends Controller
 {
@@ -36,19 +39,13 @@ class ComicController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreComicRequest $request)
     {
-        /* $form_data = $request->all(); */
-        $form_data = $this->validation($request->all());
+        $form_data = $request->all();
+        /* $form_data = $this->validation($request->all()); */
 
         $comic = new Comic();
-        $comic->title = $form_data['title'];
-        $comic->description = $form_data['description'];
-        $comic->thumb = $form_data['thumb'];
-        $comic->price = $form_data['price'];
-        $comic->series = $form_data['series'];
-        $comic->sale_date = $form_data['sale_date'];
-        $comic->type = $form_data['type'];
+        $comic->fill($form_data);
         $comic->artists = json_encode(explode(',', $form_data['artists']));
         $comic->writers = json_encode(explode(',', $form_data['writers']));
 
@@ -91,21 +88,14 @@ class ComicController extends Controller
      * @param  \App\Models\Comic  $comic
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateComicRequest $request, $id)
     {
-        /* $form_data = $request->all(); */
-        $form_data = $this->validation($request->all());
+        $form_data = $request->all();
+        /* $form_data = $this->validation($request->all()); */
 
         $comic = Comic::find($id);
 
-        $comic = Comic::find($id);
-        $comic->title = $form_data['title'];
-        $comic->description = $form_data['description'];
-        $comic->thumb = $form_data['thumb'];
-        $comic->price = $form_data['price'];
-        $comic->series = $form_data['series'];
-        $comic->sale_date = $form_data['sale_date'];
-        $comic->type = $form_data['type'];
+        $comic->update($form_data);
         $comic->artists = json_encode(explode(',', $form_data['artists']));
         $comic->writers = json_encode(explode(',', $form_data['writers']));
         $comic->update();
@@ -119,48 +109,45 @@ class ComicController extends Controller
      * @param  \App\Models\Comic  $comic
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comic $comic)
     {
-        //RECUPERO L'ELEMENTO DA CANCELLARE
-        $comic = Comic::find($id);
-
         //CANCELLO L'ELEMENTO CON IL METODO DELETE
         $comic->delete();
 
         return redirect()->route('comics.index');
     }
 
-    private function validation($data)
+    /* private function validation($data)
     {
         $validator = Validator::make(
             $data,
             [
-                'title' => 'required|max:30',
+                'title'       => 'required|max:30',
                 'description' => 'required',
-                'thumb' => 'max:255',
-                'price' => 'required',
-                'series' => 'required|max:30',
-                'sale_date' => 'required',
-                'type' => 'required|max:30',
-                'artists' => 'required',
-                'writers' => 'required',
+                'thumb'       => 'max:255',
+                'price'       => 'required',
+                'series'      => 'required|max:30',
+                'sale_date'   => 'required',
+                'type'        => 'required|max:30',
+                'artists'     => 'required',
+                'writers'     => 'required'
             ],
             [
-                'title.required' => 'Il campo Title deve essere obbligatorio',
-                'title.max' => 'Il campo Title può avere al massimo 30 caratteri',
+                'title.required'       => 'Il campo Title deve essere obbligatorio',
+                'title.max'            => 'Il campo Title può avere al massimo 30 caratteri',
                 'description.required' => 'Il campo Description deve essere obbligatorio',
-                'thumb.max' => 'Il campo Thumb può avere al massimo 255 caratteri',
-                'price.required' => 'Il campo Price deve essere obbligatorio',
-                'series.required' => 'Il campo Series deve essere obbligatorio',
-                'series.max' => 'Il campo Series può avere al massimo 30 caratteri',
-                'sale_date.required' => 'Il campo Sale Date deve essere obbligatorio',
-                'type.required' => 'Il campo Type deve essere obbligatorio',
-                'type.max' => 'Il campo Type può avere al massimo 30 caratteri',
-                'artists.required' => 'Il campo Artists deve essere obbligatorio',
-                'writers.required' => 'Il campo Writers deve essere obbligatorio'
+                'thumb.max'            => 'Il campo Thumb può avere al massimo 255 caratteri',
+                'price.required'       => 'Il campo Price deve essere obbligatorio',
+                'series.required'      => 'Il campo Series deve essere obbligatorio',
+                'series.max'           => 'Il campo Series può avere al massimo 30 caratteri',
+                'sale_date.required'   => 'Il campo Sale Date deve essere obbligatorio',
+                'type.required'        => 'Il campo Type deve essere obbligatorio',
+                'type.max'             => 'Il campo Type può avere al massimo 30 caratteri',
+                'artists.required'     => 'Il campo Artists deve essere obbligatorio',
+                'writers.required'     => 'Il campo Writers deve essere obbligatorio'
             ]
         )->validate();
 
         return $validator;
-    }
+    } */
 }
---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'       => 'required|max:30',
+            'description' => 'required',
+            'thumb'       => 'max:255',
+            'price'       => 'required',
+            'series'      => 'required|max:30',
+            'sale_date'   => 'required',
+            'type'        => 'required|max:30',
+            'artists'     => 'required',
+            'writers'     => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required'       => 'Il campo Title deve essere obbligatorio',
+            'title.max'            => 'Il campo Title può avere al massimo 30 caratteri',
+            'description.required' => 'Il campo Description deve essere obbligatorio',
+            'thumb.max'            => 'Il campo Thumb può avere al massimo 255 caratteri',
+            'price.required'       => 'Il campo Price deve essere obbligatorio',
+            'series.required'      => 'Il campo Series deve essere obbligatorio',
+            'series.max'           => 'Il campo Series può avere al massimo 30 caratteri',
+            'sale_date.required'   => 'Il campo Sale Date deve essere obbligatorio',
+            'type.required'        => 'Il campo Type deve essere obbligatorio',
+            'type.max'             => 'Il campo Type può avere al massimo 30 caratteri',
+            'artists.required'     => 'Il campo Artists deve essere obbligatorio',
+            'writers.required'     => 'Il campo Writers deve essere obbligatorio'
+        ];
+    }
+}
